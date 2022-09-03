5cb04b5fb475e1bd8684e5e6ca0b86fb87c240f7
andrematheus2 <am_arao@hotmail.com>
2022-09-03 13:54:01 -0300

    salvando pedido

---
+++ src/main/java/br/com/andre/mvc/mudi/controller/PedidoController.java
@@ -0,0 +1,30 @@
+package br.com.andre.mvc.mudi.controller;
+
+import br.com.andre.mvc.mudi.dto.RequisicaoNovoPedido;
+import br.com.andre.mvc.mudi.model.Pedido;
+import br.com.andre.mvc.mudi.repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("pedido")
+public class PedidoController {
+
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    @GetMapping("formulario")
+    public String formulario(){
+        return "pedido/formulario";
+    }
+
+    @PostMapping("novo")
+    public String novo(RequisicaoNovoPedido requisicao){
+        Pedido pedido = requisicao.toPedido();
+        pedidoRepository.save(pedido);
+        return "pedido/formulario";
+    }
+}
