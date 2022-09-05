f920924f1137063bba17569344ef92fd0938aaf8
andrematheus2 <am_arao@hotmail.com>
2022-09-05 17:36:26 -0300

    implementando o Logout

--- src/main/java/br/com/andre/mvc/mudi/WebSecurityConfig.java
+++ src/main/java/br/com/andre/mvc/mudi/WebSecurityConfig.java
@@ -1,5 +1,4 @@
 package br.com.andre.mvc.mudi;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,33 +6,36 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
-                )
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
                 .formLogin(form -> form
                         .loginPage("/login")
                         .permitAll()
-                );
-        return http.build();
+                )
+                .logout(logout -> logout.logoutUrl("/logout"));
     }
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("root")
-                .password("root")
-                .roles("ADM")
-                .build();
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("root")
+                        .password("root")
+                        .roles("ADM")
+                        .build();
+
         return new InMemoryUserDetailsManager(user);
     }
-
 }
