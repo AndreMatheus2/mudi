327de395b4269b9104663930ffef9ff2ed1c2959
andrematheus2 <am_arao@hotmail.com>
2022-09-06 10:23:34 -0300

    ajustando a navegação

--- src/main/java/br/com/andre/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/andre/mvc/mudi/controller/HomeController.java
@@ -31,22 +31,8 @@ public class HomeController {
 //        pedido.setDescricao("Digital Edition");
 //        List<Pedido> pedidos = Arrays.asList(pedido);
 
-        List<Pedido> pedidos = pedidoRepository.findAll();
+        List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE);
         model.addAttribute("pedidos", pedidos);
         return "home";
     }
-
-    @GetMapping("/{status}")
-    public String porStatus(@PathVariable("status") String status, Model model){
-        List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.valueOf(status.toUpperCase()));
-        model.addAttribute("pedidos", pedidos);
-        model.addAttribute("status", status);
-        return "home";
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public String onError(){
-        return "redirect:/home";
-    }
-
 }
